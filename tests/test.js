f8f6c780c16fca69c403647ab411102d155a6b86
Eric Glover <etgt28@mail.missouri.edu>
2017-07-31 18:00:28 -0500

    assignment complete ?

--- tests/test.js
+++ tests/test.js
@@ -5,21 +5,51 @@ module.exports = {
   "Testing the Viking Code School": browser => {
     browser
       .url(browser.launchUrl)
+      //main page
       .waitForElementVisible("body", 1000)
-      .saveScreenshot("../screenshot.png")
+      .saveScreenshot("./screenshots/main.png")
       .click("#loki-navbar > ul > li:nth-child(3) > a")
-      .saveScreenshot("../screenshot.png")
+      //sigin page
+      .saveScreenshot("./screenshots/login.png")
       .waitForElementVisible("body", 1000)
-      .setValue(
-        "#new_user > div.form-inputs > div.input.email.optional.user_email",
-        "Vlad"
-      )
-      .setValue(
-        "#new_user > div.form-inputs > div.input.password.optional.user_password",
-        "Zelinschi"
-      )
+      .setValue("#user_email", env.userEmail)
+      .setValue("#user_password", env.userPassword)
+      .waitForElementVisible("body", 1000)
       .click("#new_user > div.form-actions > input")
-      // .assert.containsText("h1", "New User")
+      //dashboard
+      .waitForElementVisible("body", 1000)
+      .saveScreenshot("./screenshots/dashboard.png")
+      /*drill down into the menu*/
+      //click that radio
+      .waitForElementVisible(
+        "ui-view[ng-if='dashboard()'] label:last-child",
+        1000
+      )
+      .click("ui-view[ng-if='dashboard()'] label:last-child", () => {
+        console.log("things were clicked");
+      })
+      .saveScreenshot("./screenshots/buttonClicked.png")
+      .click("#testing-javascript > div.unit-header > a > div > img")
+      .waitForElementVisible(
+        'a[href="#/testing-javascript/assignment-viking-code-school-exploration"]',
+        1000
+      );
+    browser
+      .click(
+        'a[href="#/testing-javascript/assignment-viking-code-school-exploration"]',
+        () => {
+          console.log("clicked it!");
+        }
+      )
+      .waitForElementVisible("body", 1000)
+      .click(".checkbox-image")
+      .saveScreenshot("./screenshots/completedCheckBox.png")
+      .click(".user-icon")
+      .waitForElementVisible("li.menu-item.last", 1000)
+      .click("li.menu-item.last")
+      //logged out
+      .waitForElementVisible("body", 1000)
+      .saveScreenshot("./screenshots/loggedOut.png")
       .end();
   }
 };
