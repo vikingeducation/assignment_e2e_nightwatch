c924b6087c09936e5da6b07118382ebe8a9fec93
Will Timpson <will.timpson@gmail.com>
2017-07-31 17:21:14 -0400

    mostly cooking... like, cooking on a wood stove... a nice one?

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,18 @@
-// Your code here ...
+env = require("../env");
 
-module.exports = {};
+module.exports = {
+  "Load the site and save a screenshot": browser => {
+    browser
+      .url("http://www.vikingcodeschool.com")
+      .waitForElementVisible("body", 1000)
+      .saveScreenshot("./screenshots/1.jpg")
+      .click('a[href="/sign_in"')
+      .saveScreenshot("./screenshots/2.jpg")
+      .setValue("#user_email", env.username)
+      .setValue("#user_password", env.password)
+      .submitForm("#new_user")
+      .pause(1000)
+      .saveScreenshot("./screenshots/3.jpg")
+      .end();
+  }
+};
