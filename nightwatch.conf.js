eb51f07bbbb51d689b4ad6b148244fa4ecc5e109
Ian Halverson <halversoncode@gmail.com>
2017-07-31 19:28:41 -0400

    finished

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,31 +1,31 @@
-const path = require('path');
-const serverPath = path.resolve('./bin/selenium-server-standalone-3.4.0.jar');
+const path = require("path");
+const serverPath = path.resolve("./bin/selenium-server-standalone-3.4.0.jar");
 
 module.exports = {
-  src_folders: ['tests'],
-  output_folder: 'reports',
+  src_folders: ["tests"],
+  output_folder: "reports",
 
   selenium: {
     start_process: true,
     server_path: serverPath,
-    port: 4444
+    port: 4445
   },
 
   test_settings: {
     default: {
-      launch_url: 'http://localhost:3000',
-      selenium_port: 4444,
-      selenium_host: 'localhost',
+      launch_url: "https://www.vikingcodeschool.com",
+      selenium_port: 4445,
+      selenium_host: "localhost",
       silent: true,
       screenshots: {
         enabled: true,
-        path: 'screenshots'
+        path: "screenshots"
       }
     },
 
     chrome: {
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: "chrome"
       }
     }
   }
--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,40 @@
-// Your code here ...
-
-module.exports = {};
+module.exports = {
+  "VCS Explore": browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible("#loki-navbar", 1000)
+      .saveScreenshot("screenshot/1.jpg")
+      .click("a[href='/sign_in']")
+      .saveScreenshot("screenshot/2.jpg")
+      .setValue("input[id='user_email']", "IDHalverson@live.com")
+      .setValue("input[id='user_password']", "Genesis1!")
+      .click("input[type='submit']")
+      .waitForElementVisible("#loki-navbar", 4000)
+      .saveScreenshot("screenshot/3.jpg")
+      .click("ui-view[ng-if='dashboard()'] label:last-child")
+      .click("unit[id='testing-javascript']")
+      .waitForElementVisible(
+        'ui-view[ng-if="dashboard()"] a[href="#/testing-javascript/assignment-viking-code-school-exploration"]',
+        4000
+      )
+      .click(
+        'ui-view[ng-if="dashboard()"] a[href="#/testing-javascript/assignment-viking-code-school-exploration"]'
+      )
+      .waitForElementVisible(
+        "div.lesson-explanation div.lesson-completion img:first-child",
+        2000
+      )
+      .pause(5000)
+      .click("div.lesson-explanation div.lesson-completion img:first-child")
+      .waitForElementVisible(
+        "img[src='https://s3.amazonaws.com/viking_education/icons/lesson_icons/icon_project_complete.svg']",
+        2000
+      )
+      .pause(5000)
+      .saveScreenshot("screenshot/4.jpg")
+      .click("img[class='user-icon']")
+      .click("a[href='/sign_out']")
+      .saveScreenshot("screenshot/5.jpg")
+      .end();
+  }
+};
