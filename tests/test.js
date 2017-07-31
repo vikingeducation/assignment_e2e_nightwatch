193f1e6ae3e03c56cf8b475979ae38fdcb8d4a7c
Eric Glover <etgt28@mail.missouri.edu>
2017-07-31 16:17:03 -0500

    init commit

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,25 @@
 // Your code here ...
+const env = require("../env.js");
 
-module.exports = {};
+module.exports = {
+  "Testing the Viking Code School": browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible("body", 1000)
+      .saveScreenshot("../screenshot.png")
+      .click("#loki-navbar > ul > li:nth-child(3) > a")
+      .saveScreenshot("../screenshot.png")
+      .waitForElementVisible("body", 1000)
+      .setValue(
+        "#new_user > div.form-inputs > div.input.email.optional.user_email",
+        "Vlad"
+      )
+      .setValue(
+        "#new_user > div.form-inputs > div.input.password.optional.user_password",
+        "Zelinschi"
+      )
+      .click("#new_user > div.form-actions > input")
+      // .assert.containsText("h1", "New User")
+      .end();
+  }
+};
