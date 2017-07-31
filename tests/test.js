201da14b128ddee261a99ee537886cf94b70d06f
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-31 18:05:25 -0400

    how to click href?

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,18 @@
 // Your code here ...
 
-module.exports = {};
+module.exports = {
+  'VCS test': browser => {
+    browser
+      .url(browser.launchUrl)
+      .saveScreenshot('screenshots/1.jpg')
+      .click('@sign_in')
+      .saveScreenshot('screenshots/2.jpg')
+
+
+
+
+
+
+      .end();
+  }
+}
