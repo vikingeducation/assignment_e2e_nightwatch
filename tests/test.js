23ac29eb86b891a2c359e8253ea7195041145a92
Chuck Michael <chuckqs@yahoo.com>
2017-09-09 11:56:28 -0400

    Solution outline but in not working state

--- tests/test.js
+++ tests/test.js
@@ -10,14 +10,14 @@ module.exports = {
     .saveScreenshot('screenshots/2.jpg')
     .waitForElementVisible('.user_email', 1000)
     //–––––––––––––add email–––––––––––––
-    .setValue('input[name="user[email]"]', '')
+    .setValue('input[name="user[email]"]', 'LOGINEMAILHERE')
     //–––––––––––––add password–––––––––––––
-    .setValue('input[name="user[password]"]', '')
+    .setValue('input[name="user[password]"]', 'LOGINPASSWORDHERE')
     .submitForm('#new_user')
-    .waitForElementPresent('.track-label-wrapper', 15000)
-    .pause(10000)
-    .waitForElementPresent( 'input[value="javascript"]', 1000)
-    .click('input[value="javascript"]')
+    .waitForElementVisible('.track-label-wrapper:not([display="none"])', 17000)
+    .pause(4000)
+    .waitForElementPresent( '#student-dashboard > input[value="javascript"]', 1000)
+    .click('#student-dashboard > input[value="javascript"]')
     .useXpath() // every selector now must be xpath
     .waitForElementPresent("//strong[contains(text(), '2: Testing JavaScript')", 2000)
     .click("//strong[contains(text(), '2: Testing JavaScript')")
@@ -28,12 +28,11 @@ module.exports = {
     .click(".checkbox-image")
     .saveScreenshot('screenshots/3.jpg')
     //–––––––––––––add full name after title–––––––––––––
-    .click("img[title='']")
+    .click("img[title='FULLNAMEHERE']")
     .waitForElementPresent(".menu-item-icon", 2000)
     .click(".menu-item-icon")
     .waitForElementVisible('.nav-right', 5000)
-    .saveScreenshot('screenshots/4.jpg'
-
+    .saveScreenshot('screenshots/4.jpg')
     .end();
   }
 };
