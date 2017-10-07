04c674f231379ad8ed90bd6a133aa8add2d47da7
Coelacanth7 <galford151@gmail.com>
2017-10-06 18:07:11 -0700

    can sign in

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,19 @@
 // Your code here ...
+const secret = require("../secret");
 
-module.exports = {};
+module.exports = {
+	"VCS test": browser => {
+		browser
+			.url(browser.launchUrl)
+			.waitForElementVisible(".language-bash ", 3000)
+			.saveScreenshot("screenshots/1.jpg")
+			.click('a[href="/sign_in"')
+			.saveScreenshot("screenshots/2.jpg")
+			.setValue('input[name="user[email]"]', secret.email)
+			.setValue('input[name="user[password]"]', secret.password)
+			.submitForm("#new_user")
+			.waitForElementVisible("#angular-loki", 3000)
+			.saveScreenshot("screenshots/3.jpg")
+			.end();
+	}
+};
