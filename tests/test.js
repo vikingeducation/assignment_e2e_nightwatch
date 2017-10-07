2e3457988723cd42ce0251a4778c08859460b90d
Coelacanth7 <galford151@gmail.com>
2017-10-06 20:39:34 -0700

    cant get radio clicked

--- tests/test.js
+++ tests/test.js
@@ -12,8 +12,23 @@ module.exports = {
 			.setValue('input[name="user[email]"]', secret.email)
 			.setValue('input[name="user[password]"]', secret.password)
 			.submitForm("#new_user")
-			.waitForElementVisible("#angular-loki", 3000)
+			.waitForElementVisible("#angular-loki", 5000)
 			.saveScreenshot("screenshots/3.jpg")
+			.waitForElementPresent("#student-dashboard", 20000)
+			.useXpath()
+			.waitForElementPresent(
+				'//*[@id="student-dashboard"]/div/div[2]/div[2]/div/div/label[3]/input',
+				10000
+			)
+			.click(
+				//this doesnt click
+				'//*[@id="student-dashboard"]/div/div[2]/div[2]/div/div/label[3]/input'
+			)
+			.waitForElementVisible(
+				'//*[@id="testing-javascript"]/div[1]/a/h3/strong',
+				5000
+			)
+			.click('//*[@id="testing-javascript"]/div[1]/a/h3/strong')
 			.end();
 	}
 };
